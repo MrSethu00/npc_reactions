ce4e0e793356b2f7da152fd49a49f44a22bf6c8f
MrSethu00 <100898307+MrSethu00@users.noreply.github.com>
2026-07-11 20:12:25 +0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sethu'
+description 'NPC Reaction System with Job Exemptions'
+version '3.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+dependencies {
+    'es_extended'
+}
